419818214b236831885e51d94638bb34cf128051
Sarvex ☠ Jatasra <sarvex.jatasra@gmail.com>
2023-10-01 10:30:31 +0530

    Update Sync

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
+import mdx from "@astrojs/mdx";
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [partytown(), mdx(), prefetch(), sitemap(), tailwind()]
+});
---
+++ prettier.config.mjs
@@ -0,0 +1,23 @@
+// prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
+
+/** @type {import("prettier").Config} */
+const config = {
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true,
+  printWidth: 100,
+  bracketSameLine: true,
+  astroAllowShorthand: true,
+
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro',
+      },
+    },
+  ],
+}
+
+export default config
